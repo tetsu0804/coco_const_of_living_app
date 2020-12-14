92f01938c87e048dd7a023ae879feee729165261
tetsu <banginhoodyonezawa@gmail.com>
2020-12-14 23:16:19 +0900

    config/application.rb と rspecインストール完了

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module CocoConstOfLivingApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        controller_specs: false
+    end
   end
 end
