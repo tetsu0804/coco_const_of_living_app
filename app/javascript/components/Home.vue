a29d462ebb0f0e5cdded79b5b0f733b8b38d3801
tetsu <banginhoodyonezawa@gmail.com>
2021-01-13 15:54:12 +0900

    Home画面の値段表示、月切り替え、月表示 完成 次は 子componenntを作成する

--- app/javascript/components/Home.vue
+++ app/javascript/components/Home.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-  home
-    <router-link to='/new_category' exact tag='button'>new Category </router-link>
+
+    <b-row>
+      <b-col class="mt-5 h3 text-center text-white">{{yearAndMonth(num)[1]}}の合計金額 ({{ yearAndMonth(num)[0]}})</b-col>
+    </b-row>
+
+    <b-row>
+      <b-col cols="2" md="3" class="d-flex align-items-center justify-content-end">
+        <b-icon icon="arrow-left-circle-fill" scale="3" variant="white" v-on:click="nextMonth" v-if="nextHideOrDisplay"></b-icon>
+      </b-col>
+      <b-col cols="8" md="6" class="text-center">
+        <b-col class="display-2 text-white">{{ priceMonthGoodsArray(num)}}円</b-col>
+      </b-col>
+      <b-col cols="2" md="3" class="d-flex align-items-center  justify-content-start">
+        <b-icon icon="arrow-right-circle-fill" scale="3" variant="white" v-on:click="prevMonth" v-if="prevHideOrDisplay"></b-icon>
+      </b-col>
+    </b-row>
+
     <b-row v-if="success_message">
       <b-col><b-alert show variant="success">{{ success_message }}</b-alert></b-col>
     </b-row>
@@ -11,11 +26,16 @@
     </b-row>
 
     <b-row>
-      <b-col sm="12" offset-md="1" md="10" offset-lg="2" lg="8">
+      <b-col sm="12" offset-md="1" md="10" offset-lg="2" lg="8" class="mt-5">
           <b-button block class="add-btn" v-b-modal.modal-no-backdrop   variant="success">追加</b-button>
       </b-col>
     </b-row>
 
+    <b-row>
+      <b-col offset="7" offset-xl="8" class="mt-2">
+        <router-link to="#" class="text-white" style="text-decoration: none;">{{ monthLastGoods(num) }}</router-link>
+      </b-col>
+    </b-row>
     <GoodsCreateModal
       :homeUseCategory="homeUseCategory"
       :selected="selected"
@@ -26,6 +46,9 @@
       v-on:childSuccessMessage="MessageSuccess"
       v-on:childErrorMessage="MessageError"
     ></GoodsCreateModal>
+    <b-row>
+      <router-link to='/new_category' exact tag='button' class="bg-success text-white border-white rounded">new Category </router-link>
+    </b-row>
 
   </div>
 </template>
@@ -47,11 +70,30 @@
         goods_price: '',
         dateValue: '',
         success_message: '',
-        error_message: ''
+        error_message: '',
+        this_date: new Date,
+        this_month: null,
+        this_year: null,
+        num: 0,
       }
     },
-    computed: mapGetters(['homeUseCategory']),
+    created() {
+
+    },
+    computed: {
+      prevHideOrDisplay() {
+        return this.prv = this.preveTrueOrFalse(this.num)
+      },
+      nextHideOrDisplay() {
+        if(this.num === 0) {
+          return false
+        } else {
+          return true
+        }
+      },
+      ...mapGetters(['homeUseCategory', 'monthGoodsArray', 'priceMonthGoodsArray', 'monthLastGoods', 'yearAndMonth', 'preveTrueOrFalse'])},
     mounted() {
+this.preveTrueOrFalse(this.num)
     },
     methods: {
       MessageSuccess(e) {
@@ -59,6 +101,12 @@
       },
       MessageError(e) {
         this.error_message = e
+      },
+      prevMonth() {
+        this.num ++
+      },
+      nextMonth() {
+        this.num --
       }
     }
   }
--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -30,6 +30,102 @@ export default new Vuex.Store({
         categories.push(valueText)
       })
       return categories
+    },
+    monthGoodsArray(state) {
+      return (this_number) => {
+        let basic_date = new Date
+        if(this_number > 0) {
+            let change_date = new Date(basic_date.setMonth(-this_number))
+          let get_month = state.goods.filter(result => {
+            let change_date_object = new Date(result.create_day)
+            let change_month = change_date_object.getMonth()
+            let change_year = change_date_object.getFullYear()
+            if(change_year === change_date.getFullYear() && change_month === change_date.getMonth()) {
+              return result
+            }
+          })
+          return get_month
+        } else {
+          let this_month_date = new Date(basic_date.setMonth(this_number))
+            let get_this_month = state.goods.filter(result => {
+
+              let date_object = new Date(result.create_day)
+
+              let this_month = date_object.getMonth()
+              let this_year = date_object.getFullYear()
+
+              if(this_year === this_month_date.getFullYear() && this_month === this_month_date.getMonth()) {
+                return result
+              }
+            })
+            return get_this_month
+        }
+      }
+    },
+    monthLastGoods(state, getters) {
+      return (this_number) => {
+        let this_month = getters.monthGoodsArray(this_number)
+
+        if(this_month.length > 0) {
+          let last_goods = this_month.slice(0, 1)
+          return `最後のお買い物は${last_goods[0].goods_name}`
+        } else {
+           return '登録されていません。'
+        }
+      }
+    },
+    yearAndMonth(state) {
+      return (this_number) => {
+        let yearOfMonthArray = []
+        let date = new Date
+        let change_date_number = date.setMonth(-this_number)
+        let change_date = new Date(change_date_number)
+        let format_year_date = 'YYYY'
+        let format_month_date = 'MM月'
+        let year_string
+        let month_string
+
+        if(this_number !== 0) {
+          year_string = change_date.getFullYear()
+          month_string = 1+ change_date.getMonth()
+
+          yearOfMonthArray.push(format_year_date.replace(/YYYY/g, year_string))
+          yearOfMonthArray.push(format_month_date.replace(/MM/g, month_string))
+        } else {
+          year_string = change_date.getFullYear()
+
+          yearOfMonthArray.push(format_year_date.replace(/YYYY/g, year_string))
+          yearOfMonthArray.push('今月')
+        }
+        return yearOfMonthArray
+      }
+    },
+    priceMonthGoodsArray(state, getters) {
+      return (this_number) => {
+          let month_total_price = 0
+          let month_goods_array = getters.monthGoodsArray(this_number)
+          month_goods_array.forEach(result => {
+            month_total_price += result.price
+          })
+          return month_total_price
+      }
+    },
+    preveTrueOrFalse(state) {
+      let today = new Date
+      let last_goods = state.goods.slice(state.goods.length -1, state.goods.length)
+      let last_goods_date = new Date(last_goods[0].create_day)
+      let last_goods_month_number = last_goods_date.getFullYear() * 12 + last_goods_date.getMonth()
+      let today_month_number = today.getFullYear() * 12 + today.getMonth()
+      let difference_month_number = today_month_number - last_goods_month_number
+
+      return (this_number) => {
+        if(this_number === difference_month_number) {
+
+          return false
+        } else {
+          return true
+        }
+      }
     }
   },
   mutations: {
