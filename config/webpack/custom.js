5d7a44e1a75c2e89dacb7520baba67facfe4da20
tetsu <banginhoodyonezawa@gmail.com>
2021-01-18 22:33:18 +0900

    config/webpack/custom.js 作成 entriPoints設定 できるかわからないが試しにやってみた

---
+++ config/webpack/custom.js
@@ -0,0 +1,6 @@
+export.default {
+  performance: {
+    maxEntrypointSize: 500000,
+    maxAssetSize: 500000,
+  }
+}
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,8 +6,9 @@ environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
 
-environment.config.merge({
-  devtool: 'cheap-source-map'
-})
+// environment.config.merge({
+//     maxEntrypointSize: 500000
+// //    maxAssetSize: 500000,
+// })
 
 module.exports = environment
