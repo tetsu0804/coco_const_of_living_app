133dbe17d02c17071594972c399e1c16e2bb86f6
tetsu <banginhoodyonezawa@gmail.com>
2021-01-18 22:45:26 +0900

    config.public_file_server.enabled=true to config/webpack/production.js へ追加してみた

--- config/webpack/custom.js
+++
@@ -1,6 +0,0 @@
-export.default {
-  performance: {
-    maxEntrypointSize: 500000,
-    maxAssetSize: 500000,
-  }
-}
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+config.public_file_server.enabled=true` # ENV['RAILS_SERVE_STATIC_FILES'].present?
+
 module.exports = environment.toWebpackConfig()
