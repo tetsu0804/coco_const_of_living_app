0aa55eee97d13dc111015579602afa8c42212dda
tetsu <banginhoodyonezawa@gmail.com>
2021-01-18 22:02:14 +0900

    performance: hints: false to config/webpack/enviroments.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,11 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.config.merge({
+  performance: {
+    hints: false
+  }
+})
+
 module.exports = environment
