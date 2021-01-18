bc00ea3849badaeb7e0091e0646d4600319d3095
tetsu <banginhoodyonezawa@gmail.com>
2021-01-18 22:54:51 +0900

    config.public_file_server.enabled=true  to config/webpack/production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-config.public_file_server.enabled=true` # ENV['RAILS_SERVE_STATIC_FILES'].present?
+config.public_file_server.enabled=true
 
 module.exports = environment.toWebpackConfig()
